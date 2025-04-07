e7322af9d5b25e3e2a6b17b38397e46fedfdeb24
jideosan <jideosan@gmail.com>
2025-04-06 21:00:42 -0400

    move images

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,6 +1,57 @@
 import { http, HttpResponse } from 'msw';
 import { data as mockSpices } from './data/spices';
 import { data as mockBlends } from './data/blends';
+import { Blend, BlendSpice } from '../types';
+
+function resolveSpices(blend: Blend, blends: Blend[]) {
+  function flattenBlendIds(
+    blend: Blend,
+    blendMap: Map<number, Blend>,
+    aggrgateBlends: number[] = [],
+  ) {
+    aggrgateBlends = [...aggrgateBlends, blend.id];
+
+    blend.blend_ids.forEach((id) => {
+      if (!aggrgateBlends.includes(id)) {
+        const deepBlend = blendMap.get(id);
+        if (!deepBlend) throw Error('Invalid blend id:' + id);
+        aggrgateBlends = flattenBlendIds(deepBlend, blendMap, aggrgateBlends);
+      }
+    });
+
+    return aggrgateBlends;
+  }
+
+  const blendMap = new Map(blends.map((b) => [b.id, b]));
+  const includedBlendsIds = flattenBlendIds(blend, blendMap);
+
+  const resolvedSpices: Map<number, BlendSpice> = new Map();
+  const spiceMap = new Map(mockSpices().map((s) => [s.id, s]));
+
+  includedBlendsIds.forEach((id) => {
+    const includedBlend = blendMap.get(id);
+    if (!includedBlend) throw Error('Invalid blend id:' + id);
+    includedBlend.spice_ids.forEach((id) => {
+      const includedBlendSpice = resolvedSpices.get(id);
+      if (includedBlendSpice) {
+        resolvedSpices.set(id, {
+          ...includedBlendSpice,
+          blend_ids: [...includedBlendSpice.blend_ids, includedBlend.id],
+        });
+      } else {
+        const newIncludedBlendSpice = spiceMap.get(id);
+        if (newIncludedBlendSpice) {
+          resolvedSpices.set(id, {
+            ...newIncludedBlendSpice,
+            blend_ids: [newIncludedBlendSpice.id],
+          });
+        }
+      }
+    });
+  });
+
+  return Array.from(resolvedSpices.values());
+}
 
 export const handlers = [
   http.get('/api/v1/spices', ({ request }) => {
@@ -39,11 +90,29 @@ export const handlers = [
       return new HttpResponse('Not found', { status: 404 });
     }
 
-    return HttpResponse.json(spice);
+    return HttpResponse.json({ data: spice });
   }),
 
-  http.get('/api/v1/blends', () => {
-    return HttpResponse.json(mockBlends());
+  http.get('/api/v1/blends', ({ request }) => {
+    const url = new URL(request.url);
+    const search = url.searchParams.get('search')?.toLowerCase() || '';
+
+    const blends = mockBlends();
+
+    let filteredBlends = search
+      ? blends.filter((blend) => blend.name.toLowerCase().includes(search))
+      : blends;
+
+    filteredBlends = filteredBlends.map((blend) => {
+      return {
+        ...blend,
+        resolved_spices: resolveSpices(blend, blends),
+      };
+    });
+
+    return HttpResponse.json({
+      data: filteredBlends,
+    });
   }),
 
   http.post('/api/v1/blends', () => {
@@ -51,12 +120,18 @@ export const handlers = [
   }),
 
   http.get('/api/v1/blends/:id', ({ params }) => {
-    const blend = mockBlends().find((blend) => blend.id === Number(params.id));
+    const blends = mockBlends();
+    const blend = blends.find((blend) => blend.id === Number(params.id));
 
     if (!blend) {
       return new HttpResponse('Not found', { status: 404 });
     }
 
-    return HttpResponse.json(blend);
+    return HttpResponse.json({
+      data: { 
+        ...blend, 
+        resolved_spices: resolveSpices(blend, blends),
+      },
+    });
   }),
 ];
